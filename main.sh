a1753eda82ee3f07b3dd2f97f63052542951f692
Chris Ho <chrisho@fb.com>
2024-11-09 16:40:16 -0800

    move python files to library

--- main.sh
+++ main.sh
@@ -7,6 +7,6 @@ fi
 # Activate the virtual environment
 source coffee-chats/bin/activate
 # Run the python script with the provided arguments
-python3 main.py "$1" "$2"
+python3 lib/main.py "$1" "$2"
 # Deactivate the virtual environment
 deactivate
